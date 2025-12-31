a5ecdbb8af3eb9ad0f7046025598bd5bef3cbf82
KANNICHA ARRUK <s6804062612374@email.kmutnb.ac.th>
2025-12-31 11:37:07 +0700

    N grade version 2

---
+++ lab4no9.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+    int n,score,i;
+    scanf("%d",&n);
+    while (1==1){
+        scanf("%d",&score);
+        if (score == -1){
+            break;
+        }
+        else if (score>100){
+            printf("error");
+        }
+        else if (score<0){
+            printf("error");
+        }
+        else if (score>=68){
+            if (score>=85){
+                printf("%d(A)\n",score);
+            }
+            else if (score>=75){
+                printf("%d(B)\n",score);
+            }
+            else if (score>=68){
+                printf("%d(C)\n",score);
+            }
+        }
+        else if (score>=55){
+            printf("%d(D)\n",score);
+        }
+        else{
+            printf("%d(F)\n",score);
+        }
+    }
+}
